945023b6eec6cfea6006ecd7dab093c34f46bd71
Jonatas Siqueira <jonatassilvasiqueira663@gmail.com>
2024-08-15 03:03:17 +0000

    começo do mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,64 @@
-import fundo from '../assets/fundoHome.png';
+
 import { Conteudo } from '../styles/Home.styles';
 import { Video } from '../styles/Home.styles';
 import logoImg from "../assets/logoSunflower.png";
-
+import fundo from '../assets/fundoHome.png';
+import closeIcon from "../assets/icones/menuMobile/close.png";  
+import menuOpenIcon from "../assets/icones/menuMobile/menuOpen.png"; 
 import { Link } from 'react-router-dom';
 
-
-
-
-
-
-
 export function Home() {
+  const menuShow = () => {
+    const menuMobile = document.querySelector('.mobile-menu');
+    const icon = document.querySelector('.icon');
+    if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      icon.src = menuOpenIcon;  
+    } else {
+      menuMobile.classList.add('open');
+      icon.src = closeIcon ;  
+    }
+  };
+
   return (
     <Conteudo>
       <header>
-        <nav>
-          <a href="#"><img src={logoImg} className="logo" alt="Logo da SunFlower" /></a>
-          <ul>
-
-            <a href="#"><li>Home</li></a>
-            <a href="#publico"><li>Público-alvo</li></a>
-            <a href="#Carrossel"><li>Peças</li></a>
-            <Link to="/grup"><li>Grupo</li> </Link> 
-          </ul>
+        <nav className="nav-bar">
+          <div className="logo">
+            <img src={logoImg} alt="Logo da SunFlower" />
+          </div>
+          <div className="nav-list">
+            <ul>
+              <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
+              <li className="nav-item"><a href="#publico" className="nav-link">Público-alvo</a></li>
+              <li className="nav-item"><a href="#Carrossel" className="nav-link">Peças</a></li>
+              <li className="nav-item"><Link to="/grup" className="nav-link">Grupo</Link></li>
+            </ul>
+          </div>
+          <div className="login-button">
+            <button><a href="#">Entrar</a></button>
+          </div>
+          <div className="mobile-menu-icon">
+            <button onClick={menuShow}><img className="icon" src={menuOpenIcon} alt="Menu Icon" /></button>
+          </div>
         </nav>
+        <div className="mobile-menu">
+          <ul>
+            <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#publico" className="nav-link">Público-alvo</a></li>
+            <li className="nav-item"><a href="#Carrossel" className="nav-link">Peças</a></li>
+            <li className="nav-item"><Link to="/grup" className="nav-link">Grupo</Link></li>
+          </ul>
+          <div className="login-button">
+            <button><a href="#">Entrar</a></button>
+          </div>
+        </div>
       </header>
       <div className="home" style={{ backgroundImage: `url(${fundo})` }}>
         <h1>Rastreador solar unidirecional de fluxo fotovoltaico</h1>
         <p>Rastreamento Solar Unilateral: Maximizando a Eficiência Energética para Sistemas Fotovoltaicos</p>
       </div>
+      
       <ShortVideo />
     </Conteudo>
   );
@@ -38,7 +67,6 @@ export function Home() {
 const ShortVideo = () => {
   return (
     <Video>
-
       <div className="short-video">
         <iframe
           width="660"
@@ -50,12 +78,6 @@ const ShortVideo = () => {
           allowFullScreen
         ></iframe>
       </div>
-
     </Video>
-
-
   );
 };
-
-
-
--- src/styles/Home.styles.js
+++ src/styles/Home.styles.js
@@ -1,86 +1,172 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const Conteudo = styled.div`
-    header {
-        position: fixed;
-        background: ${props => props.theme.HomeCorFundo};
-        padding: 5px 10px; 
-        width: 100%;
-        z-index: 1000;
-        height: 50px; 
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Inter', sans-serif; /* Fonte conforme o CSS original */
+  }
+
+  header {
+    background-color: #00040D; /* Cor de fundo do cabeçalho */
+    box-shadow: 0px 3px 10px #464646; /* Sombra do cabeçalho */
+  }
+
+  .nav-bar img {
+    padding: 0;
+    height: 50px; 
+    width: auto;
+  }
+
+  .nav-bar {
+    display: flex;
+    justify-content: space-between;
+    padding: 0; /* Padding conforme o CSS original */
+    align-items: center;
+  }
+
+  .nav-list {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-list ul {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+  }
+
+  .nav-item {
+    margin: 0 15px; /* Margem dos itens do menu */
+  }
+
+  .nav-link {
+    text-decoration: none;
+    font-size: 1.15rem; /* Tamanho da fonte dos links do menu */
+    color: #fff; /* Cor dos links do menu */
+    font-weight: 400;
+  }
+
+  .login-button button {
+    border: none;
+    padding: 10px 15px;
+    border-radius: 5px;
+    background-color: #0187a7; /* Cor do botão de login */
+  }
+
+  .login-button button a {
+    text-decoration: none;
+    color: #fff; /* Cor do texto do botão de login */
+    font-weight: 500;
+    font-size: 1.1rem;
+  }
+
+  .mobile-menu-icon {
+    display: none;
+  }
+
+  .mobile-menu {
+    display: none;
+  }
+
+  .home {
+    margin-bottom: 3rem;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    color: #DFDEFF;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 80px;
+    padding: 0 20px;
+  }
+
+  .home h1 {
+    margin: 0 auto;
+    max-width: 800px;
+    font-size:  50px; /* Tamanho da fonte no desktop */
+  }
+
+  .home p {
+    margin: 0 auto;
+    font-size: 25px; /* Tamanho da fonte no desktop */
+    max-width: 700px;
+  }
+
+  @media screen and (max-width: 730px) {
+    .nav-bar {
+      padding: 1.5rem 2rem; /* Ajusta o padding para telas menores */
     }
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 100%; 
+    .nav-item {
+      display: none;
     }
-    ul {
-        display: flex;
-        padding: 0; 
-        list-style-type: none;
-        margin: 0;
+    .login-button {
+      display: none;
     }
-    li {
-        color: #ffff;
-        margin: 0 20px;
-        font-size: 20px; 
-        cursor: pointer;
-        transition: color 0.3s ease;
+    .mobile-menu-icon {
+      display: block;
     }
-    li:hover {
-        color: #938E8E;
+    .mobile-menu-icon button {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
     }
-    .logo {
-        padding: 0;
-        height: 50px; 
-        width: auto;
+    .mobile-menu ul {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      padding-bottom: 1rem;
     }
-    .home {
-        font-size: 2rem;
-        margin-bottom: 3rem;
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        min-height: 100vh;
-        color: #DFDEFF;
-        text-align: center;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding-top: 80px;
-        padding: 0 20px;
+    .mobile-menu .nav-item {
+      display: block;
+      padding-top: 1.2rem;
     }
+    .mobile-menu .login-button {
+      display: block;
+      padding: 1rem 2rem;
+    }
+    .mobile-menu .login-button button {
+      width: 100%;
+    }
+    .open {
+      display: block;
+    }
+
     .home h1 {
-        margin: 0 auto;
-        max-width: 800px;
+      margin: 0 auto;
+      font-size: 2.2rem /* Ajusta o tamanho da fonte no mobile */
     }
+
     .home p {
-        margin: 0 auto;
-        font-size: 2.5rem;
-        max-width: 700px;
+      margin: 0 auto;
+      font-size: 1.7rem; /* Ajusta o tamanho da fonte no mobile */
+      max-width: 100%; /* Faz o parágrafo ocupar toda a largura disponível */
     }
-    a {
-        text-decoration: none;
-    }
-    .grup{
-        color: #ffff;
-        margin: 0 20px;
-        font-size: 20px; 
-        cursor: pointer;
-        transition: color 0.3s ease;
-    }
-    
-`
+  }
+`;
 
 export const Video = styled.div`
-
- .short-video{
+  .short-video {
     display: flex;
     justify-content: center;
     padding-top: 20px;
- }
+  }
 
-`
+  @media (max-width: 730px) {
+    .short-video {
+      width: 100%;
+      height: auto;
+      flex-direction: column;
+      align-items: center;
+    }
+    .short-video iframe {
+      width: 100%;
+      height: auto;
+      max-width: 300px; /* Ajusta a largura máxima do vídeo no mobile */
+    }
+  }
+`;
