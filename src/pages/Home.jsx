981a902f7031e97f88c06f77fbc6407888858f8c
jonatassilvasiqueira663 <jonatassilvasiqueira663@gmail.com>
2024-11-04 14:11:18 +0000

    finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import { Conteudo } from '../styles/Home.styles';
 import { Video } from '../styles/Home.styles';
 import fundo from '../assets/fundoHome.png';
 import PHeader from './Header';
+import videoPath from '../assets/ImgBenePage/video.mp4'; // Primeiro vídeo
+import segundoVideoPath from '../assets/ImgBenePage/videoProjeto.mp4'; // Segundo vídeo
 
 export function Home() {
-  
-
   return (
     <Conteudo>
       <PHeader/>
@@ -15,25 +15,20 @@ export function Home() {
         <p>Rastreamento Solar Unilateral: Maximizando a Eficiência Energética para Sistemas Fotovoltaicos</p>
       </div>
       
-      <ShortVideo />
-      <ShortVideo />
+      <ShortVideo src={videoPath} />
+      <ShortVideo src={segundoVideoPath} /> {/* Usando a variável correta */}
     </Conteudo>
   );
 }
 
-const ShortVideo = () => {
+const ShortVideo = ({ src }) => {
   return (
     <Video>
       <div className="short-video">
-        <iframe
-          width="750"
-          height="420"
-          src="https://www.youtube.com/embed/BadB1z-V_qU?si=FBkYkarDLO4dFwwH"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <video width="750" height="420" controls>
+          <source src={src} type="video/mp4" />
+          Seu navegador não suporta a tag de vídeo.
+        </video>
       </div>
     </Video>
   );
